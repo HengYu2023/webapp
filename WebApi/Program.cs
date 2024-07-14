6a5969e6223f785a6edea9c5822b7b9db1a79020
Arthur-Liu <positive0324@gmail.com>
2024-07-15 00:02:40 +0800

    改使用 .Net 1or2 program 與 startup

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,35 +3,49 @@ using CoreBusiness.Service;
 using DataAccess.EFRepository;
 using DataAccess.Entities;
 using DataAccess.Interface;
+using Microsoft.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using WebApi;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var str = builder.Configuration["ConnectionStrings:NorthwindConnection"];
-
-builder.Services.AddDbContext<NorthwindContext>(options=> options.UseSqlServer(str));
-
-builder.Services.AddScoped<IRepository<Product>,EFRepository<Product>>();
-builder.Services.AddScoped<IRepository<Category>,EFRepository<Category>>();
-builder.Services.AddScoped<ICategoryService,CategoryService>();
-builder.Services.AddScoped<IProductService,ProductService>();
-
-builder.Services.AddControllers();
-
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    
+    public static void Main(string[] args)
+    {
+        CreateWebHostBuilder(args).Build().Run();
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>();
 }
 
-app.UseHttpsRedirection();
+//var builder = WebApplication.CreateBuilder(args);
 
-app.UseAuthorization();
+//var str = builder.Configuration["ConnectionStrings:NorthwindConnection"];
 
-app.MapControllers();
+//builder.Services.AddDbContext<NorthwindContext>(options=> options.UseSqlServer(str));
 
-app.Run();
+//builder.Services.AddScoped<IRepository<Product>,EFRepository<Product>>();
+//builder.Services.AddScoped<IRepository<Category>,EFRepository<Category>>();
+//builder.Services.AddScoped<ICategoryService,CategoryService>();
+//builder.Services.AddScoped<IProductService,ProductService>();
+
+//builder.Services.AddControllers();
+
+
+//var app = builder.Build();
+
+//if (app.Environment.IsDevelopment())
+//{
+
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
 
 
