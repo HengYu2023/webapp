da641b4c5ff84851b4577f62e2d2d010ea0cf56d
Arthur <arthur_liu@mayohr.com>
2024-07-24 13:32:11 +0800

    Add Dto and ViewModel

---
+++ CoreBusiness/Dto/CategoryDto.cs
@@ -0,0 +1,12 @@
+namespace CoreBusiness.Dto;
+
+public class CategoryDto
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+}
